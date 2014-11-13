8cd74daa57c0156dfc5c9d966147a87c40963d21
Kevin Attfield <kevin@freerunningtech.com>
2014-11-12 21:16:36 -0800

    Randomize banner text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var chooseRandom = function(array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+var byline = chooseRandom(window.bylines);
+$('.ribbon').text(byline);
