7f71971f5521c5e212347bf5a3457d535ba0c4c2
Kevin Attfield <kevin@freerunningtech.com>
2014-11-12 21:05:25 -0800

    Add answers

---
+++ app/scripts/answers.js
@@ -0,0 +1,31 @@
+window.answers = [{
+    text: 'Yes!',
+    explanation: 'Each moment you\'re not with pizza is a moment you\'re without pizza. Get on your phone or whatever and sort it out. Go.'
+  },{
+    text: 'Yep.',
+    explanation: 'Pizza is ultra-low effort and at some locations, suspciously affordable. Either way, you\'re doing the right thing.'
+  },{
+    text: 'Absolutely.',
+    explanation: 'Pizza is the magical fruit. Put it inside you. You want it.'
+  },{
+    text: 'Hells Yes.',
+    explanation: 'Pizza contains all of the food groups. It is perfect. It is sacred. It is Beautiful.'
+  },{
+    text: 'Affirmative.',
+    explanation: 'How could this even be a question? Why does this website even exist? C\'mon. Stop it.'
+  },{
+    text: 'Roger That.',
+    explanation: 'If you counted up all the pizzas in history, you probably wouldn\'t be able to. That\'s how many there would be. History has spoken here.'
+  },{
+    text: 'Correct.',
+    explanation: 'In ancient times, the Romans would eat pizza at their orgies to give them good luck. "Good luck to you", they would say and cheers each other with their slices, as was the custom.'
+  },{
+    text: 'Totally.',
+    explanation: 'Say what you want about Mary Kate and Ashley Olsen, but when they slowed down that "Gimme Pizza" song it turned out pretty good. Give yourself pizza. P-I-Z-Z-A.'
+  },{
+    text: 'Might As Well.',
+    explanation: 'Let\'s be honest, you were going to order the pizza no matter what we told you. What\'s that saying about your self-image? It\'s time to do some soul searching. Time to start up some Qi Gong maybe.'
+  },{
+    text: 'Nope.',
+    explanation: 'You asked and now the universe has presented you with this unfortunate outcome. Sorry it has to be this way. It\'s going to be OK. Maybe cook up some parsnips or have a curry'
+}];
